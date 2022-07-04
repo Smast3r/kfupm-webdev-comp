bf666203c51b845469bc2eedfcf0e175655c9abb
Saad Alshrideh <saadalshrideh@Saads-Mac-mini.local>
2022-07-04 16:23:45 +0300

    tries

--- src/components/AddCourse.tsx
+++ src/components/AddCourse.tsx
@@ -1,70 +1,99 @@
-import { getValue } from "@testing-library/user-event/dist/utils"
-import { useEffect, useState } from "react"
-import Course from "./Course"
+import { getValue } from "@testing-library/user-event/dist/utils";
+import { useContext, useEffect, useState } from "react";
+import { TermsContext } from "../contexts/TermsContext";
+import Course from "./Course";
 
-
-type props= {
-    setVisablity:any
-    setCourses:any
-}
+type props = {
+  setVisablity: any;
+  setCourses: any;
+  termNumber: number;
+};
 type Course = {
-    hours:number,
-    withLab:boolean,
-    name:string,
-}
+  hours: number;
+  withLab: boolean;
+  name: string;
+};
 
-const AddCourse = (Props:props)=>{
+const AddCourse = (Props: props) => {
+  const { terms, setTerms } = useContext(TermsContext);
+  const [Term, setTerm] = useState(
+    terms?.filter((value) => value.termNumber == Props.termNumber)
+  );
+  const [newCourse, setNewCourse] = useState<Course>({
+    withLab: false,
+  } as Course);
 
-    const [newCourse ,setNewCourse] = useState<Course>({withLab:false}as Course)
+  useEffect(() => {
+    document.getElementById("AddButton")?.focus();
+  }, []);
+  const handleSubmition = (
+    Event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    Event.preventDefault();
+    Props.setCourses((prev: Course[]) => {
+      return [...prev, newCourse];
+    });
+    Props.setVisablity(false);
+  };
+  return (
+    <div className=" overflow-y-auto overflow-x-hidden rounded  mx-auto  absolute z-50 inset-x-0 top-0 w-full h-full bg-transparent flex justify-center items-center">
+      <div className="sm:w-2/4 md:w-1/4 bg-zinc-700 grid gird-rows-5 sm:h-2/4 md:h-2/4 items-center rounded-xl">
+        <form>
+          <div className="relative p-4 w-full h-full md:h-auto flex justify-center">
+            <div className="mx-4"> name </div>
+            <input
+              type="text"
+              onChange={(e) => {
+                setNewCourse((prev) => {
+                  return { ...prev, name: e.target.value } as Course;
+                });
+              }}
+            />
+          </div>
+          <div className="relative p-4 w-full  h-full md:h-auto flex justify-center">
+            <div className="mx-4">Hours</div>
+            <input
+              type="number"
+              onChange={(e) => {
+                setNewCourse((prev) => {
+                  return { ...prev, hours: Number(e.target.value) } as Course;
+                });
+              }}
+            />
+          </div>
 
-    useEffect(()=>{
-        document.getElementById('AddButton')?.focus()
-        
-    },[])
-    const handleSubmition = (Event:React.MouseEvent<HTMLButtonElement, MouseEvent>)=>{
-        Event.preventDefault()
-        console.log(newCourse)
-        Props.setVisablity(false)
-        Props.setCourses((prev:Course[])=>[...prev,newCourse])
+          <div className="relative p-4 w-full  h-full md:h-auto flex justify-center">
+            <div className="mx-4">Lab ?</div>
+            <input
+              type="checkbox"
+              onChange={(e) => {
+                setNewCourse((prev) => {
+                  return {
+                    ...prev,
+                    withLab: Boolean(e.target.value),
+                  } as Course;
+                });
+              }}
+            />
+          </div>
 
+          <div className="flex justify-center">
+            <button
+              id="AddButton"
+              type="submit"
+              className="w-48 bg-cyan-600 h-10 rounded"
+              onClick={(e) => {
+                handleSubmition(e);
+              }}
+            >
+              {" "}
+              Add{" "}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-
-    }
-    return(
-        <div className=" overflow-y-auto overflow-x-hidden rounded  mx-auto  absolute z-50 inset-x-0 top-0 w-full h-full bg-transparent flex justify-center items-center" >
-            <div className="sm:w-2/4 md:w-1/4 bg-zinc-700 grid gird-rows-5 sm:h-2/4 md:h-2/4 items-center rounded-xl">
-                <form>
-                <div className="relative p-4 w-full h-full md:h-auto flex justify-center"> 
-                    <div className="mx-4"> name </div>
-                    <input type="text"  onChange={(e)=>{
-                        setNewCourse((prev)=> {return {...prev,name:e.target.value}as Course})
-                    }}/>
-                </div>
-                <div className="relative p-4 w-full  h-full md:h-auto flex justify-center"> 
-                    <div className="mx-4">Hours</div>
-                    <input type="number" onChange={(e)=>{
-                        setNewCourse((prev)=> {return {...prev,hours:Number(e.target.value)}as Course})
-                    }}/>
-                </div>
-                
-                <div className="relative p-4 w-full  h-full md:h-auto flex justify-center"> 
-                    <div className="mx-4">Lab ?</div>
-                    <input  type="checkbox" onChange={(e)=>{
-                        setNewCourse((prev)=> {return {...prev,withLab:Boolean(e.target.value)}as Course})
-                    }}
-                     />
-                </div>
-
-                <div className="flex justify-center">
-                <button id="AddButton" type="submit" className="w-48 bg-cyan-600 h-10 rounded" onClick={(e)=>{handleSubmition(e)}}> Add </button>
-                </div>
-                </form>
-            </div>
-        </div>
-    )
-
-
-}
-
-
-export default AddCourse
+export default AddCourse;
--- src/components/Term.tsx
+++ src/components/Term.tsx
@@ -1,62 +1,68 @@
-
-import { useState } from "react"
-import AddCourse from "./AddCourse"
-import AddCourseButton from "./AddCourseButton"
-import Course from "./Course"
+import { useContext, useEffect, useState } from "react";
+import { TermsContext } from "../contexts/TermsContext";
+import AddCourse from "./AddCourse";
+import AddCourseButton from "./AddCourseButton";
+import Course from "./Course";
 
 type Props = {
-number:number
-Courses:Course[]
-} 
+  number: number;
+  Courses: Course[];
+};
 type Course = {
-    hours:number,
-    withLab:boolean,
-    name:string,
-}
+  hours: number;
+  withLab: boolean;
+  name: string;
+};
 type Term = {
-    termNumber:number,
-    termCourses?:Course[]
-}
+  termNumber: number;
+  termCourses?: Course[];
+};
 
-const Term = (props:Props)=> {
-    const [visablity,setVisablity] = useState(false);
-    const [courses,setCourses] = useState(props.Courses)
-    const showModal=()=>{
-        setVisablity(true)
-        
-    }
-    return (
-        <>
-        <div className="grid grid-cols-1 gap-2 align-middle max-h-max bg-zinc-900 rounded p-2 text-cyan-200 w-52">
-            <div className="flex justify-center">
-                <h1>{props.number}</h1>
-            </div>
-            
-            <>
-                {
-                courses.map((course,index)=>{
-                    <Course key={index} hours={course.hours} name={course.name} withLab={course.withLab} />
-                })
-                }
-            </>
-            
+const Term = (props: Props) => {
+  const [visablity, setVisablity] = useState(false);
+  const [courses, setCourses] = useState(props.Courses);
+  const { terms, setTerms } = useContext(TermsContext); // i should use the term instade of the things
+  const showModal = () => {
+    setVisablity(true);
+  };
+  const termNumber = props.number;
 
-            <AddCourseButton          
-                showModal={showModal}
-            >Add Course</AddCourseButton>
+  return (
+    <>
+      <div className="grid grid-cols-1 gap-2 align-middle max-h-max bg-zinc-900 rounded p-2 text-cyan-200 w-52">
+        <div className="flex justify-center">
+          <h1>{props.number}</h1>
         </div>
-        {
-            visablity?
-            <AddCourse setCourses={setCourses} setVisablity={setVisablity}/>:
-            null
-        }
 
+        <>
+          {terms
+            ?.filter((term) => term.termNumber == termNumber)[0]
+            .termCourses?.map((course, index) => {
+              <Course
+                key={index}
+                hours={course.hours}
+                name={course.name}
+                withLab={course.withLab}
+              />;
+            })}
         </>
-    )
+        <button
+          className="h-10 w-10 bg-cyan-100"
+          onClick={() => {
+            console.log(courses);
+          }}
+        ></button>
+        <AddCourseButton showModal={showModal}>Add Course</AddCourseButton>
+      </div>
+      {visablity ? (
+        <AddCourse
+          setCourses={setCourses}
+          setVisablity={setVisablity}
+          termNumber={termNumber}
+        />
+      ) : null}
+    </>
+  );
+};
 
-}
-
-
-
-
-export default Term
+export default Term;
